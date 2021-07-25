a64e24918483199f880b0e5c7c1455aa1ad06f97
heayr <79095992+heayr@users.noreply.github.com>
2021-07-25 22:34:29 +0300

    style edits and optimized imgs mark_3.1

--- scripts/index.js
+++ scripts/index.js
@@ -1,39 +1,37 @@
-const popupEdit = document.querySelector('.profile__info_edit-button');
-const popupClose = document.querySelector('.popup__close');
+const popupEdit = document.querySelector(".profile__info_edit-button");
+const popupClose = document.querySelector(".popup__close");
 // думал как реализовать закрытие если нажать кнопкой мыши вне попапа, пока не придумал
 // const popupCloseBack = document.querySelector('.popup__opened');
 
-const popup = document.querySelector('.popup');
+const popup = document.querySelector(".popup");
 
-const popupContent = document.querySelector('.popup__container');
+const popupContent = document.querySelector(".popup__container");
 
 function togglePopup(event) {
   event.stopPropagation();
-  popup.classList.toggle('popup__opened');
+  popup.classList.toggle("popup__opened");
 }
 
-popupEdit.addEventListener('click', togglePopup);
-popupClose.addEventListener('click', togglePopup);
+popupEdit.addEventListener("click", togglePopup);
+popupClose.addEventListener("click", togglePopup);
 // popupCloseBack.addEventListener('click', togglePopup);
-
 
 // save
 
-
-let formElement = document.querySelector('.popup__container');
-let nameInput = formElement.querySelector('.popup__name');
-let statusInput = formElement.querySelector('.popup__status');
+let formElement = document.querySelector(".popup__container");
+let nameInput = formElement.querySelector(".popup__name");
+let statusInput = formElement.querySelector(".popup__status");
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
 
-  let nameChange = document.querySelector('.profile__title');
+  let nameChange = document.querySelector(".profile__title");
   nameChange.textContent = nameInput.value;
 
-  let jobChange = document.querySelector('.profile__subtitle');
+  let jobChange = document.querySelector(".profile__subtitle");
   jobChange.textContent = statusInput.value;
 
   // popupClose();
 }
 
-formElement.addEventListener('submit', formSubmitHandler);
+formElement.addEventListener("submit", formSubmitHandler);
