6c177d35268ca4734b5428581ff2a70b0209c54a
heayr <79095992+heayr@users.noreply.github.com>
2021-07-26 21:18:06 +0300

    edits of css and html mark_5.1

--- scripts/index.js
+++ scripts/index.js
@@ -4,14 +4,15 @@ const popup = document.querySelector(".popup");
 let formElement = document.querySelector(".popup__container");
 let nameInput = formElement.querySelector(".popup__input_name");
 let statusInput = formElement.querySelector(".popup__input_status");
-let nameChange = document.querySelector(".profile__title");
-let jobChange = document.querySelector(".profile__subtitle");
+let profileName = document.querySelector(".profile__title");
+let statusChange = document.querySelector(".profile__subtitle");
 
 
 
 function togglePopup(event) {
   event.stopPropagation();
   popup.classList.toggle("popup__opened");
+
 }
 
 popupEdit.addEventListener("click", togglePopup);
@@ -19,11 +20,14 @@ popupClose.addEventListener("click", togglePopup);
 // save
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  nameChange.textContent = nameInput.value;
-  jobChange.textContent = statusInput.value;
+  profileName.textContent = nameInput.value;
+  statusChange.textContent = statusInput.value;
+  // строки снизу как будто ничего не делают
+  nameInput.value = profileName .textContent;
+  statusInput.value = statusChange.textContent;
 
-  // closePopup();
-
+  popup.classList.toggle("popup__opened");
+  // оказалось добавить toggle сюда же в функцию самое простое
 
   // document.getElementsByClassName('popup__opened')[0].remove();
 }
