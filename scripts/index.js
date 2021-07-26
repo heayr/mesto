3c874b8007c60c3a561c6d35cbbd0ce85a223d67
heayr <79095992+heayr@users.noreply.github.com>
2021-07-26 23:36:49 +0300

    edits JS single action functions and saveInfo mark_6

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,4 @@
+// переменные
 const popupEdit = document.querySelector(".profile__edit-button");
 const popupClose = document.querySelector(".popup__close");
 const popup = document.querySelector(".popup");
@@ -6,34 +7,32 @@ let nameInput = formElement.querySelector(".popup__input_name");
 let statusInput = formElement.querySelector(".popup__input_status");
 let profileName = document.querySelector(".profile__title");
 let statusChange = document.querySelector(".profile__subtitle");
+console.log(profileName);
+console.log(statusChange);
 
-
-
-function togglePopup(event) {
+// Функция открытия popup и копирования данных из титула и подтитула
+function openPopup(event) {
   event.stopPropagation();
-  popup.classList.toggle("popup__opened");
-
+  popup.classList.add('popup__opened')
+  nameInput.value = profileName.textContent;
+  statusInput.value = statusChange.textContent;
 }
+// Функция закрытия popup
+function removePopup(event) {
+  event.stopPropagation();
+  popup.classList.remove('popup__opened')
+}
+popupEdit.addEventListener("click", openPopup);
+popupClose.addEventListener("click", removePopup);
 
-popupEdit.addEventListener("click", togglePopup);
-popupClose.addEventListener("click", togglePopup);
-// save
+
+
+// save and close of popup info
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   statusChange.textContent = statusInput.value;
-  // строки снизу как будто ничего не делают
-  nameInput.value = profileName .textContent;
-  statusInput.value = statusChange.textContent;
-
-  popup.classList.toggle("popup__opened");
-  // оказалось добавить toggle сюда же в функцию самое простое
-
-  // document.getElementsByClassName('popup__opened')[0].remove();
+  popup.classList.remove("popup__opened");
 }
-
-// let text = document.querySelector('.')
-// document.querySelector('profile__title').textContent = '';
-
 
 formElement.addEventListener("submit", formSubmitHandler);
