3d65bbcb07567239b51b2cbf12e4a7c35778a53a
heayr <79095992+heayr@users.noreply.github.com>
2021-07-28 22:45:37 +0300

    added names for inputs, adaptive imgs, mark_8

--- scripts/index.js
+++ scripts/index.js
@@ -34,14 +34,3 @@ function formSubmitHandler(evt) {
 popupEdit.addEventListener("click", openPopup);
 popupClose.addEventListener("click", removePopup);
 formElement.addEventListener("submit", formSubmitHandler);
-
-
-//  Вопрос, написал код, он работает, а как в итоге понять, что он работает не так, если в консоле нет ошибок и код исполняется типа правильно.
-
-// Как с этим было
-
-// " let formElement = document.querySelector(".popup__container");
-// Тут должна быть форма." В итоге я понял, что я не ту обертку выбрал, нужно всегда выбирать ту,
-//  что ближе .popup__form явно будет родителем импутов, поэтому лучше его взять
-
-// Может помимо документации есть что-то, что стоит почитать в когда-нибудь свободное время, чтобы лучше понять JS? или лцше с практикой закреплять всё?
