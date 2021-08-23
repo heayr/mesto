417a059e81ac8805cb4fedf723e606a12a65deb5
heayr <79095992+heayr@users.noreply.github.com>
2021-08-23 23:24:01 +0300

    comments mark_5.3

--- scripts/validation.js
+++ scripts/validation.js
@@ -1,3 +1,5 @@
+// показать в инпуте ошибку
+// Находим элемент ошибки внутри самой функции
 const showInputError = (formElement, inputElement, errorMessage, classes) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
 
@@ -5,14 +7,15 @@ const showInputError = (formElement, inputElement, errorMessage, classes) => {
   errorElement.textContent = errorMessage;
   errorElement.classList.add(classes.errorClass);
 };
-
+// скрыть ошибку в инпуте
+// Находим элемент ошибки
 const hideInputError = (formElement, inputElement, classes) => {
   const errorElement = formElement.querySelector(`#${inputElement.id}-error`);
   inputElement.classList.remove(classes.inputErrorClass);
   errorElement.classList.remove(classes.errorClass);
   errorElement.textContent = '';
 };
-
+// проверить валидность
 const checkInputValidity = (formElement, inputElement, classes) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage, classes);
@@ -22,6 +25,18 @@ const checkInputValidity = (formElement, inputElement, classes) => {
 };
 
 
+// функция проверки массива инпутов
+const hasInvalidInput = (inputList) => {
+  // проходим по этому массиву методом some
+  return inputList.some((inputElement) => {
+    // Если поле не валидно, колбэк вернёт true
+    // Обход массива прекратится и вся фунцкция
+    // hasInvalidInput вернёт true
+    return !inputElement.validity.valid;
+  })
+};
+
+// тогл кнопки "сохранить" связана с функцией hasInvalidInput, которая проверяет есть хоть одно невалидное совпадение
 const toggleButtonState = (inputList, buttonElement, inactiveButtonClass) => {
   if (hasInvalidInput(inputList)) {
     buttonElement.classList.add(inactiveButtonClass);
@@ -32,17 +47,12 @@ const toggleButtonState = (inputList, buttonElement, inactiveButtonClass) => {
   }
 };
 
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  })
-}
-
+// функция обновления валидации в инпутах и создание массива инпутов
 function setEventListeners(formElement, classes) {
   const inputList = Array.from(formElement.querySelectorAll(classes.inputSelector));
   const buttonElement = formElement.querySelector(classes.submitButtonSelector);
 
-  // toggleButtonState(inputList, buttonElement, classes.inactiveButtonClass);
+  // toggleButtonState(inputList, buttonElement, classes.inactiveButtonClass); в данном случае не будем блокировать кнопку с самого начала
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', function () {
@@ -52,19 +62,24 @@ function setEventListeners(formElement, classes) {
   });
 }
 
-
+// Функция enableValidation найдёт на странице и обработает все формы с классом '.popup__form'
+// Найдём все формы с указанным классом в DOM,
+// сделаем из них массив методом Array.from
 const enableValidation = (classes) => {
   const formList = Array.from(document.querySelectorAll(classes.formSelector));
-
+  // Переберём полученную коллекцию
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (evt) => {
+      // У каждой формы отменим стандартное поведение
       evt.preventDefault();
     });
+    // Для каждой формы вызовем функцию setEventListeners,
+    // передав ей элемент формы
     setEventListeners(formElement, classes);
   })
 }
 
-
+// Вызовем функцию с заданными параметрами классов назовем - classes
 enableValidation({
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
